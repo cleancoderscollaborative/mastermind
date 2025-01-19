8707716ac68f7f4f2eae38a5d3cc023f16d6304b
Joel Mussman <jmussman@smallrock.net>
2025-01-19 14:00:34 -0500

    Transformation to MVC

--- src/controller/HomeController.py
+++ src/controller/HomeController.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from src.service.MessageService import MessageService
 
 class HomeController:
@@ -9,15 +11,4 @@ class HomeController:
         @app.route("/")
         def hello_world():
 
-            return f'''
-            <html>
-            <head>
-                <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico" />
-            </head>
-            <body>
-                <div style="background-color: lightgray; text-align: center; min-height: calc(100vh - (100vh - 300px) / 2); padding-top: calc((100vh - 300px) / 2);">
-                    <img src="/assets/images/clean-code.png" /><h1>{self.message_service.get_message('hello')}</h1>
-                </div>
-            </body>
-            </html>
-            '''
+            return render_template('page/home.html', message = self.message_service.get_message('hello'))
--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from src.service.MessageService import MessageService
 
 load_dotenv()
 
-app = Flask(__name__, static_folder='../static', static_url_path='/assets')
+app = Flask(__name__, static_folder = '../static', static_url_path = '/assets', template_folder = './view')
 message_service = MessageService(messages)
 homeController = HomeController(app, message_service)
 
@@ -19,18 +19,12 @@ domain = os.getenv('CODESPACE_NAME')
 
 def start():
 
-    debug = False
-
-    if sys.gettrace() is not None:
-
-        debug = True
-
     if domain:
         print(f"Application starting on port https://{domain}-{service_port}.app.github.dev/")
     else:
         print(f"Application starting on http://localhost:{service_port}/")
 
-    app.run(debug = debug, port = service_port)
+    app.run(port = service_port)
 
 if __name__ == '__main__':
 
