5874d215de4c2eb9a8d5d07ff8a6a24ccf13b3dd
Joel Mussman <jmussman@smallrock.net>
2025-01-20 01:03:34 -0500

    Added controller unit tests

--- src/controller/HomeController.py
+++ src/controller/HomeController.py
@@ -1,14 +1,13 @@
 from flask import render_template
 
-from src.service.MessageService import MessageService
-
 class HomeController:
 
     def __init__(self, app, message_service):
 
         self.message_service = message_service
                 
-        @app.route("/")
-        def hello_world():
+        @app.route('/')
+        def home():
 
-            return render_template('page/home.html', message = self.message_service.get_message('hello'))
+            result = render_template('page/home.html', message = self.message_service.get_message('hello'))
+            return result
