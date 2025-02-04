f1ccd6965e39c1ee80bbdc35b028bbab717f9866
Joel Mussman <jmussman@W21082201.local>
2025-02-04 17:47:49 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -7,33 +7,26 @@ from src.SayHello.Domain.messageModel import messages
 from src.SayHello.ApplicationLogic.MessageService import MessageService
 
 def initialize() -> Flask:
+    global _home_controller
 
     load_dotenv()
-
-    # The template folder is the source folder so that pages can be placed in the feature folders.
-
     app = Flask(__name__, static_folder = './FluxServerFramework/assets', static_url_path = '/assets', template_folder = '.')
-
     message_service = MessageService(messages)
-
-    global _home_controller  # This protects the controller from garbage collection by placing the reference in global space.
     _home_controller = HomeController(app, message_service)
 
     return app
 
 def start():
-
     app = initialize()
-
-    service_port = getenv('SERVICEPORT')
-    domain = getenv('CODESPACE_NAME')           # The service is Codespace-aware.
-
-    if domain:
-        print(f"Application starting on port https://{domain}-{service_port}.app.github.dev/")
-    else:
-        print(f"Application starting on http://localhost:{service_port}/")
-
+    (service_port, domain) = (getenv('SERVICEPORT'), getenv('CODESPACE_NAME'))
+    print_codespace_url(domain, service_port) if domain else print_url(service_port)
     app.run(port = service_port)
+
+def print_codespace_url(domain, service_port):
+    print(f"Application starting on port https://{domain}-{service_port}.app.github.dev/")
+
+def print_url(service_port):
+    print(f"Application starting on http://localhost:{service_port}/")
 
 if __name__ == '__main__':
 
